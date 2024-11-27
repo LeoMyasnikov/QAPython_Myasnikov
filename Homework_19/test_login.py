4bb0033b41f71113b1c36ae6d5321c2a56588f15
LeoMyasnikov <63258397+LeoMyasnikov@users.noreply.github.com>
2024-11-27 23:39:00 +0300

    Update test_login.py
    Добавил url отдельной переменной

--- Homework_19/test_login.py
+++ Homework_19/test_login.py
@@ -1,9 +1,14 @@
 from selenium.webdriver.common.by import By
+
+url = 'https://demo.applitools.com/#'
+
+
 def test_login_chrome(driver_chrome):
     """Тест авторизации на сайте Applitools Demo в Chrome."""
     driver = driver_chrome
-    driver.get("https://demo.applitools.com/#")
+    driver.get(url)
 
+    
     # Заполнение формы авторизации
     driver.find_element(By.ID, "username").send_keys("admin")
     driver.find_element(By.ID, "password").send_keys("admin")
@@ -11,10 +16,11 @@ def test_login_chrome(driver_chrome):
 
     assert "ACME demo app" in driver.title
 
+
 def test_login_firefox(driver_firefox):
     """Тест авторизации на сайте Applitools Demo в Firefox."""
     driver = driver_firefox
-    driver.get("https://demo.applitools.com/#")
+    driver.get(url)
 
     # Заполнение формы авторизации
     driver.find_element(By.ID, "username").send_keys("admin")
